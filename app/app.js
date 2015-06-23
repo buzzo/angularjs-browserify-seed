74d5f6f0f28ed0eeb114f4d2ecaa16a1083866f4
buzzo <andre@buzzo.com.br>
2015-06-23 09:03:16 +0100

    Moving css not related to dashboard (general css) to new main css file.

--- app/app.js
+++ app/app.js
@@ -2,5 +2,6 @@ require('angular/angular');
 require('angular-route/angular-route');
 require('angular-css');
 
+// door3.css is from angular-css (adding style 'css' per route)
 angular.module('app', ['ngRoute', 'door3.css'])
     .config(require('./routes'));
