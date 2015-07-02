a34f60b7cd4b4e33d19f1b27eff6887bbef43cda
buzzo <andre@buzzo.com.br>
2015-07-02 16:22:36 +0100

    feat(lib): add angular-bootstrap
    Added angular-bootstrap library to use components

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 require('angular/angular');
 require('angular-route/angular-route');
 require('angular-css');
+require('angular-bootstrap');
 
 // door3.css is from angular-css (adding style 'css' per route)
-angular.module('app', ['ngRoute', 'door3.css']).config(['$routeProvider', require('./routes')]);
+angular.module('app', ['ngRoute', 'ui.bootstrap', 'door3.css']).config(['$routeProvider', require('./routes')]);
