a3d7434e88f1be7bf08d193d9317c49a9ac0f255
Andre Buzzo <andre.buzzo@ericsson.com>
2015-07-03 15:50:08 +0100

    feat(grid): add angular grid
    Added angular-grid to generate flexible tables. Updated example inside security feature.

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ require('angular/angular');
 require('angular-route/angular-route');
 require('angular-css');
 require('angular-bootstrap');
+require('angular-grid');
 
 // door3.css is from angular-css (adding style 'css' per route)
-angular.module('app', ['ngRoute', 'ui.bootstrap', 'door3.css']).config(['$routeProvider', require('./routes')]);
+angular.module('app', ['ngRoute', 'door3.css', 'ui.bootstrap', 'angularGrid']).config(['$routeProvider', require('./routes')]);
